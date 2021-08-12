b4ecc4a91a1446e031af2d8b43eaee7fffebc103
nimt0001 <nimtrihieu1111@gmail.com>
2021-08-12 13:16:37 +1000

    Update intersect_sim_HN.sh

--- Validation-VISTAheart-Dickel2016/intersect_sim_HN.sh
+++ Validation-VISTAheart-Dickel2016/intersect_sim_HN.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+#Randomly selected 4450 CREs from  the raw heart enhancers, repeat the simulation 1000 times
 for i in {0..1000}
 do
   shuf -n 4450 RawHeartEnhancers.bed > out/random_RawHeartEnhancers.bed
@@ -6,8 +7,3 @@ do
   bedtools intersect -a VISTAheart_liftover_mm9.sorted.bed -b out/random_RawHeartEnhancers.bed | wc -l >> out/countLines_VISTAheart.txt
 done
 
-#bedtools intersect -a Pennachio_mm9.sorted.bed -b Dang_enhancer_mm9.sorted.bed >> out/intersect_Pennacchio_Dang_enhancer.txt
-#bedtools intersect -a VISTAheart_liftover_mm9.sorted.bed -b Dang_enhancer_mm9.sorted.bed >> out/test1.txt
-#shuf -n 4450 RawHeartEnhancers.bed > out/random_RawHeartEnhancers.bed
-#bedtools intersect -a Pennacchio_mm9.sorted.bed -b out/random_RawHeartEnhancers.bed >> out/intersect_raw_Pennacchio.txt
-#bedtools intersect -a Pennacchio_mm9.sorted.bed -b out/random_RawHeartEnhancers.bed | wc -l >> out/countLines.txt
